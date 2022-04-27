88e2d436adf625da98e3ac3699af4f1fd0084c93
“ys_131554c” <810317099@qq.com>
2022-04-27 18:23:26 +0800

    spring cloud -- eureka： 服务发现 - 负载均衡

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -22,7 +22,8 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2.
-        String url = "http://localhost:8081/user/" + order.getUserId();
+        String url = "http://localhost:8082/user/" + order.getUserId();
+        //String url = "http://userservice/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         // 3.
         order.setUser(user);
